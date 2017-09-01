a38a92ebd90b15a00dada07184943cb2774b3d0a
nylaz <lars.nyman@blinfo.se>
2017-09-01 21:51:03 +0200

    Hej

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ myApp.config(function ($routeProvider) {
     $routeProvider
         .when("/Home", {
             templateUrl: "Home.html",
-            controller: "myCtrl"
+            controller: "homeController"
         })
         .when("/Ongoing_polls", {
             templateUrl: "Ongoing_polls.html",
@@ -15,16 +15,15 @@ myApp.config(function ($routeProvider) {
         })
         .when("/Poll_info/:id", {
             templateUrl: "Poll_info.html",
-            controller: "myCtrl2"
+            controller: "pollInfoController"
         })
         .otherwise({
             templateUrl: "Home.html",
             controller: "myCtrl"
         });
-
 });
 
-myApp.controller('myCtrl', function ($scope, $http, $location) {
+myApp.controller('homeController', function ($scope, $http, $location) {
     $http.get("http://iftacvoteapi.azurewebsites.net/vote/GetQuestions", {
         headers: {"Authorization": "e52f39cf-d0aa-4fee-a24c-08646747057e"}
     })
@@ -32,17 +31,20 @@ myApp.controller('myCtrl', function ($scope, $http, $location) {
             $scope.groupData = response.data;
         });
     $scope.click = function (x) {
-        console.log(x);
         $location.path("/Poll_info/" + x.QuestionId);
     }
 });
 
-myApp.controller('myCtrl2', function ($scope, $http, $routeParams) {
-    console.log($routeParams.id);
+myApp.controller('pollInfoController', function ($scope, $http, $routeParams) {
     $http.get("http://iftacvoteapi.azurewebsites.net/vote/GetQuestion?questionId=" + $routeParams.id, {
         headers: {"Authorization": "e52f39cf-d0aa-4fee-a24c-08646747057e"}
     })
         .then(function (response) {
-            $scope.groupData = response.data;
+            $scope.data = response.data;
         });
+    $scope.click = function(data) {
+        $http.get("http://iftacvoteapi.azurewebsites.net/vote/SendReminder?questionId=" + data.QuestionId, {
+            headers: {"Authorization": "e52f39cf-d0aa-4fee-a24c-08646747057e"}
+        })
+    }
 });
